2d1ca4a9cd9d52468f79be3890fb874124401831
Nathans-Codeprojects <natemail06@gmail.com>
2025-05-19 18:29:56 +0200

    Update Year_3_Project.ino

--- Year_3_Project.ino
+++ Year_3_Project.ino
@@ -1,36 +1,34 @@
 #include <LiquidCrystal_I2C.h>
-
-#include <LiquidCrystal_I2C.h>
 #include <SD.h>
-#include <Adafruit_MPU6050.h>
+#include <Adafruit_MPU6050.h>   //All libraries needed
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
-const int SD_LED2 = 9;
-const int Button = 2;
-bool ledState = false;
+const int SD_LED2 = 9;   //Led for recording
+const int Button = 2;    //interrupt button
+bool ledState = false;  //initial state of the led
 
 int ErrorCode;
 
-const int BUTTON_W = 4;
+const int BUTTON_W = 4;   //3 yellow buttons for the wrist, elbow and shoulder files/tests
 const int BUTTON_E = 5;
 const int BUTTON_S = 6;
 
 bool wristFlag = false, elbowFlag = false, shoulderFlag = false, Recordingflag = false;
 
-float ROM, maxroll;
-static bool offset_set = false;
+float ROM, maxroll; //Max ROM for the 2 different calculations
+static bool offset_set = false;     //bool in order to reset the max ROM
 static bool offset_roll_set = false;
 
 
 String Tests_W[] = {"Flexion", "Extension"};
-String Tests_E[] = {"Flexion", "Extension", "Pronation", "Supination"};
+String Tests_E[] = {"Flexion", "Extension", "Pronation", "Supination"};  //Arrays containing the test names for each effected region
 String Tests_S[] = {"Flexion", "Extension", "Adduction", "Abduction"};
 
 int index_W = 0, index_E = 0, index_S = 0;
 
 Adafruit_MPU6050 mpu;
-File myFile;
+File myFile;              //class objects
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
@@ -44,7 +42,7 @@ void setup(void)
   }
 
   // Try to initialize
-  if (!mpu.begin())
+  if (!mpu.begin())   //initialize mpu-6050
   {
     ErrorCode = 1;
     error(ErrorCode);
@@ -59,23 +57,24 @@ void setup(void)
   pinMode(BUTTON_E, INPUT_PULLUP);
   pinMode(BUTTON_S, INPUT_PULLUP);  
   
-  lcd.init();
+  lcd.init();    //initialize LCD screen
   lcd.backlight();
 
-  attachInterrupt(digitalPinToInterrupt(Button), ISR0, FALLING); // Trigger on button press
+  attachInterrupt(digitalPinToInterrupt(Button), ISR0, FALLING); // attach interupt on button press falling edge
   
-  initSD();
+  initSD(); //initialize SD card shield
 
-  initMPU();
+  initMPU(); //sets up mpu-6050 parameters
 
   Serial.println("");
   delay(500);
 
-  startUp();
+  startUp(); //inital loop to cycle untill an effected region has been selected
 
 }
 
 void loop() {
+  //open the different files depending on what button was pressed
   if(wristFlag == true)
   {
     myFile = SD.open("WRIST.txt", FILE_WRITE);
@@ -97,10 +96,12 @@ void loop() {
   float AccelY = a.acceleration.y;
   float AccelZ = a.acceleration.z;
 
+  //calculation of the roll and tilt angles
   float Angle = -atan2(-AccelX, AccelZ) * 180.0 / PI;
   float roll = atan(AccelY / sqrt(AccelX * AccelX + AccelZ * AccelZ)) * 180.0 / PI;
 
 
+  //determining the current angle when the test is started to calculate the deviation from 0
   static float roll_offset;
   static float Angle_offset;
 
@@ -121,6 +122,7 @@ void loop() {
   float relative_roll = roll - roll_offset;
   float rollnorm = abs(relative_roll);
 
+  //storing the max ROM
   if(Angle_norm >= ROM)
   {
     ROM = Angle_norm;
@@ -130,6 +132,7 @@ void loop() {
   {
     maxroll=rollnorm;
   }
+  //displaying the current acceleration data and max ROM on the LCD screen
   lcd.setCursor(0, 0);
   lcd.print("X:"); lcd.print(AccelX,1);
 
@@ -145,6 +148,7 @@ void loop() {
   Serial.print("Roll: "); Serial.print(rollnorm);  
   
   lcd.setCursor(7, 1); 
+  //showing the roll ROM for pronation and supination otherwise shows the tilt max ROM
   if(index_E == 3 || index_E == 4)
   {
     lcd.print("ROM:"); lcd.print(maxroll);  
@@ -155,16 +159,6 @@ void loop() {
     lcd.print("ROM:"); lcd.print(ROM);
     Serial.print("ROM: "); Serial.println(ROM);
   }
-
- /*
-  if(Recordingflag == true)
-  {
-    myFile.print("Angle ");
-    myFile.print(Angle_norm); myFile.print(" Roll ");
-    myFile.println(rollnorm);
-  }
-  */
-
   delay(500);
 }
 
@@ -185,9 +179,9 @@ void initSD()
 
 void initMPU()
 {
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);  //initialize mpu-6050 filter at 21Hz
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);//initialize mpu-6050 accelerometer sensitivity at 2g
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);//initialize mpu-6050 groscope sensitivity at 250deg
 }
 
 void startUp()
@@ -230,6 +224,7 @@ void startUp()
 
 void ISR0() 
 {  
+  //opens correct file when intturput button is pressed
   if (wristFlag) 
   {
     myFile = SD.open("WRIST.txt", FILE_WRITE);
@@ -246,6 +241,8 @@ void ISR0()
 
   if(ledState) 
   {
+    //ledstate is true when the intterupt button is pressed to end the test therefore, the led is turned off and the max ROM
+    //is written to the SD card
     digitalWrite(SD_LED2, LOW);
     Recordingflag = false;
     ledState = false;
@@ -261,6 +258,8 @@ void ISR0()
   } 
   else 
   {
+    //when recording should start the ROM is reset and the current test is printed to the SD card while incrementing the array
+    //inorder to move the array to the following test
     digitalWrite(SD_LED2, HIGH);    
   
     ROM = 0;
@@ -308,6 +307,7 @@ void ISR0()
   myFile.close();
 }
 
+//error function to display any error message depending on what has gone wrong in the setup of the code
 void error(int code)
 {
 
